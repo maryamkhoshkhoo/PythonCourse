4f63c6b003bcc0970202f68916871194b700dfe8
Maryam Khoshkhoo <97059979+maryamkhoshkhoo@users.noreply.github.com>
2022-03-23 11:22:45 +0430

    Delete Assignment8 directory

--- Assignment8/Gif.py
+++
@@ -1,9 +0,0 @@
-import imageio
-import os
-
-images = []
-for file_name in os.listdir('images'):
-    image = imageio.imread(f'images/{file_name}')
-    images.append(image) #poshtesarham namayesh mide axaro
-    
-imageio.mimsave('baby.gif', images)  
