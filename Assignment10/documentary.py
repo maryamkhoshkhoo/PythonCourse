beb1a4d2cc316d9f926116b35025c17373fb970a
Maryam Khoshkhoo <97059979+maryamkhoshkhoo@users.noreply.github.com>
2022-04-17 16:56:18 +0430

    Delete Assignment10 directory

--- Assignment10/documentary.py
+++
@@ -1,6 +0,0 @@
-from media import Media
-
-class Documentary(Media):
-    def __init__(self, type, name ,director ,duration, url, imdb, year, casts, p):
-        Media.__init__(self ,type, name ,director ,duration, url, imdb, year, casts)
-        self.parts = p 
