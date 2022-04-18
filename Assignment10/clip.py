580344a8ca07c53c4859c1c42f3a2d626e97a58e
Maryam Khoshkhoo <97059979+maryamkhoshkhoo@users.noreply.github.com>
2022-04-18 09:32:14 +0430

    Delete Assignment10 directory

--- Assignment10/clip.py
+++
@@ -1,5 +0,0 @@
-from media import Media
-
-class Clip(Media):
-    def __init__(self, type, name ,director ,duration, url, imdb, year, casts):
-        Media.__init__(self ,type, name ,director ,duration, url, imdb, year, casts)
